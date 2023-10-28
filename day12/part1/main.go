1c7747fdd257feec3e8a5fdc551c96d5b4aeca04
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2023-10-28 17:46:29 -0500

    day12

---
+++ day12/part1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AntonKosov/advent-of-code-2017/aoc"
+)
+
+func main() {
+	answer := process(read())
+	fmt.Printf("Answer: %v\n", answer)
+}
+
+func read() [][]int {
+	lines := aoc.ReadAllInput()
+	lines = lines[:len(lines)-1]
+	adjList := make([][]int, len(lines))
+	for i, line := range lines {
+		nums := aoc.StrToInts(line)
+		adjList[i] = nums[1:]
+	}
+
+	return adjList
+}
+
+func process(adjList [][]int) int {
+	visited := make([]bool, len(adjList))
+	visit(adjList, 0, visited)
+
+	count := 0
+	for _, v := range visited {
+		if v {
+			count++
+		}
+	}
+
+	return count
+}
+
+func visit(adjList [][]int, index int, visited []bool) {
+	if visited[index] {
+		return
+	}
+
+	visited[index] = true
+
+	for _, adjIndex := range adjList[index] {
+		visit(adjList, adjIndex, visited)
+	}
+}
